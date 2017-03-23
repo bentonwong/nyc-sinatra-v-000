0e91c3d51863f7fb82175137264b2f1ec8b6a60d
bentonwong <bentonwong@gmail.com>
2017-03-23 11:17:41 -0700

    add code to allow saving of landmark and title to figure and fix ids in new.erb

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,24 +5,29 @@ class FiguresController < ApplicationController
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
-    erb :index
+    @figures = Figure.all
+    erb :'/figures/index'
   end
 
   get '/figures' do
-    redirect to '/'
+    redirect to "/"
   end
 
   get '/figures/new' do
-    erb :'figures/new'
+    erb :'/figures/new'
   end
 
   post '/figures' do
     @figure = Figure.create(params["figure"])
     if !params["title"]["name"].empty?
       @figure.titles << Title.find_or_create_by(params["title"])
+    elsif !!params["figure"]["title_ids"]
+      @figure.titles << params["figure"]["title_ids"].map {|t_id| Title.find_by_id(t_id)}
     end
     if !params["landmark"]["name"].empty?
       @figure.landmarks << Landmark.find_or_create_by(params["landmark"])
+    elsif !!params["figure"]["landmark_ids"]
+      @figure.titles << params["figure"]["landmark_ids"].map {|l_id| Landmark.find_by_id(l_id)}
     end
     @figure.save
 
