c398a65354fbcc3ec6be9502ff661013a76641c1
bentonwong <bentonwong@gmail.com>
2017-03-22 22:25:12 -0700

    add controller action to start new form and make placeholder for new.erb

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,15 @@
 class FiguresController < ApplicationController
 
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  get '/' do
+    erb :index
+  end
+
+  get '/figures/new' do
+    erb :new
+  end
+
 end
