7c2737d0fdca72f6824b934cf52aa7145fcf21bd
bentonwong <bentonwong@gmail.com>
2017-03-23 14:42:04 -0700

    add controller to view landmarks new page

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,14 @@
+require_relative '../../config/environment'
+require 'pry'
+
 class LandmarksController < ApplicationController
+
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
 
 end
